68fd3dff11872f4f8d9304cfa05c1a4139e1ccc6
Diana <dinessa.ga@gmail.com>
2025-07-27 12:54:53 -0500

    feat: add image of figma and change image path

--- website-herux/src/components/Benefits.tsx
+++ website-herux/src/components/Benefits.tsx
@@ -1,35 +1,42 @@
 
 import React from "react";
 
+import avatar1 from "../assets/benefits/avatar1.svg";
+import avatar2 from "../assets/benefits/avatar1.svg";
+import avatar3 from "../assets/benefits/avatar1.svg";
+import avatar4 from "../assets/benefits/avatar1.svg";
+import avatar5 from "../assets/benefits/avatar1.svg";
+import avatar6 from "../assets/benefits/avatar1.svg";
+
 const benefitsData = [
   {
-    img: "/src/assets/hero/1.png",
+    image: avatar1,
     title: "Desarrollo personal",
     text: "Brindamos charlas de habilidades blandas enfocada en lo personal y profesional con especialistas en el área."
   },
   {
-    img: "/src/assets/hero/1.png",
+    image: avatar2,
     title: "Desarrollo profesional",
     text: "Brindamos participación dentro o fuera de la comunidad desde el área de tu interés. Asímismo tendrás la apertura de ser mentor para un junior, o ponente en alguna charla."
 
   },
   {
-    img: "/src/assets/hero/1.png",
+    image: avatar3,
     title: "Beneficio 3",
     text:"Descripción breve del beneficio 3."
   },
   {
-    img: "/src/assets/hero/1.png",
+    image: avatar4,
     title: "Beneficio 4",
     text: "Descripción breve del beneficio 4."
   },
   {
-    img: "/src/assets/hero/1.png",
+    image: avatar5,
     title: "Beneficio 5",
     text: "Descripción breve del beneficio 5."
   },
   {
-    img: "/src/assets/hero/1.png",
+    image: avatar6,
     title: "Beneficio 6",
     text: "Descripción breve del beneficio 6."
   }
@@ -48,7 +55,7 @@ const Benefits: React.FC = () => {
               key={idx}
             >
               <div className="w-14 h-14 m-2 flex items-center justify-center bg-base-100 rounded-full overflow-hidden">
-                <img src={benefit.img}  className="w-full h-full object-contain" />
+                <img src={benefit.image}  className="w-full h-full object-contain" />
               </div>
               <div className="flex-1 flex flex-col text-left">
                 <h3 className="text-lg font-semibold text-accent mb-2">{benefit.title}</h3>
